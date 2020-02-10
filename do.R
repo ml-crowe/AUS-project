24ee75bbe5e61e2dbdcf451d7f86796c855f8b37
Michael Crowe <michael.l.crowe@gmail.com>
2020-02-09 19:43:08 -0500

    add do file

---
+++ do.R
@@ -0,0 +1,17 @@
+#### 1. Descriptives and Alphas -------------------
+
+#___1.1 Demographics ----------
+df$sex %>% table
+df$ethnicity %>% table
+df$race %>% table
+df$marital %>% table
+df$ever_psyc %>% table
+df$current_psyc %>% table
+df$device %>% table
+
+#___1.2 NEO ----------
+df[,paste('r_neo_',c(1+(5*0:11)), sep = '')] %>% alpha #N = .85
+df[,paste('r_neo_',c(2+(5*0:11)), sep = '')] %>% alpha #E = .85
+df[,paste('r_neo_',c(3+(5*0:11)), sep = '')] %>% alpha #O = .63
+df[,paste('r_neo_',c(4+(5*0:11)), sep = '')] %>% alpha #A = .73
+df[,paste('r_neo_',c(5+(5*0:11)), sep = '')] %>% alpha #C = .87
