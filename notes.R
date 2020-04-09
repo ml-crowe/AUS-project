74cf4ed8dc7ddafd65453a1295af0200d3df803c
Michael Crowe <michael.l.crowe@gmail.com>
2020-04-09 12:03:30 -0400

    removed resolved issues.

--- notes.R
+++ notes.R
@@ -7,7 +7,3 @@
 # current problems requiring resolution -----------------------------------
 
 
-# The read qualtrics function will accurately identify the Date Time structure of the primary qulatrics dataset, but not the pilot data. 
-# Manually setting date time; strptime(dat$StartDate,format = '%m/%d/%y %H:%M'); is necessary for the pilot data to be imported accurately, but it turns all of the date values for the primary dataset to NA. Trying to force the pilot dataset import to match the primary dataset specifications; spec <- spec(df); col_types = spec; yields misspecification error.
-
-# wrong.code object is no longer being appropriately generated
