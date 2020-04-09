301c4178c5a3933da0b35736583c650314256f67
Michael Crowe <michael.l.crowe@gmail.com>
2020-04-09 12:02:27 -0400

    initial factor analyses

--- do.R
+++ do.R
@@ -15,3 +15,8 @@ df[,paste('r_neo_',c(2+(5*0:11)), sep = '')] %>% alpha #E = .85
 df[,paste('r_neo_',c(3+(5*0:11)), sep = '')] %>% alpha #O = .63
 df[,paste('r_neo_',c(4+(5*0:11)), sep = '')] %>% alpha #A = .73
 df[,paste('r_neo_',c(5+(5*0:11)), sep = '')] %>% alpha #C = .87
+
+#### 2. Factor Analyses -------------------
+aus <- select(df, aus_1:aus_70)
+parallel <- fa.parallel(aus, fm = 'pa', fa = 'both', n.iter = 100)
+#Parallel analysis suggests that the number of factors =  6  and the number of components =  4 
