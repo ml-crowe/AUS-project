c091e2448b0b550c8e1e98abc2cff514498dce09
Michael Crowe <michael.l.crowe@gmail.com>
2020-05-01 14:15:58 -0400

    identify additional items to be removed

--- notes.R
+++ notes.R
@@ -7,3 +7,6 @@
 # current problems requiring resolution -----------------------------------
 
 
+# We want to drop item #67; 45: "If I tell my friends I'm mad, they will probably avoid me in the future"
+# Remove any items with a negative wording
+# Pull items 
