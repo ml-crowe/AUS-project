47205ccb51d354a22b298adac37e61b5feda714a
Michael Crowe <michael.l.crowe@gmail.com>
2020-06-22 15:01:54 -0400

    updated comments

--- notes.R
+++ notes.R
@@ -5,8 +5,14 @@
 # 
 
 # current problems requiring resolution -----------------------------------
+# We retained item 56 in round 3 of item removal, but its item level fit was poor
+# Following round 3 of item removal, its item-level fit was acceptable
+# Don't know how to interpret that
 
+# empirical plots show some mild to significant misfit for some items
+# we may want to consider evaluating a different model? maybe GPCM?
 
-# We want to drop item #67; 45: "If I tell my friends I'm mad, they will probably avoid me in the future"
-# Remove any items with a negative wording
-# Pull items 
+# Look at item correlations: make sure there are none that are redundant
+# 
+# I think only the singlets in the adjusted MODFIT file is super important - that's what I
+# would interpret based on my psychopathy curve paper
